7dccebb6695be9a20177e3ba776da114670919b5
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-02-06 09:51:54 -0500

    initial commit

---
+++ src/test/java/com/GeorgesServer/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package com.GeorgesServer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestHelloWorld {
+
+    @Test
+    void thisTestShouldFail() {
+        assertTrue(true);
+    }
+}
