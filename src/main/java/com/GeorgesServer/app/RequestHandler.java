0ada542cc11f6ef53b1ce18da55a64cefa134aa6
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-02-14 09:30:30 -0500

    first request handler test is passing

--- src/main/java/com/GeorgesServer/app/RequestHandler.java
+++ src/main/java/com/GeorgesServer/app/RequestHandler.java
@@ -3,7 +3,10 @@ package com.GeorgesServer.app;
 import java.util.HashMap;
 
 public class RequestHandler {
-    public String handle(HashMap<String, String> mockedClientRequest) {
+    public String handle(HashMap<String, String> clientRequest) {
+        if (clientRequest.get("Method").equals("GET") && clientRequest.get("Url").equals("/")) {
+            return clientRequest.get("HttpVersion") + " 200 OK" + "\n";
+        }
         return "";
     }
 }
