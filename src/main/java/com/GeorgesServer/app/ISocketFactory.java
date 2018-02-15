de6b633781d87cf2ebefe886672e54cd64ca1354
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-02-15 09:46:07 -0500

    remove unneeded interface

--- src/main/java/com/GeorgesServer/app/ISocketFactory.java
+++
@@ -1,9 +0,0 @@
-package com.GeorgesServer.app;
-
-import java.net.ServerSocket;
-import java.net.Socket;
-
-public interface ISocketFactory {
-    ServerSocket createSocket(int port);
-    Socket createClientSocket(ServerSocket serverSocket);
-}
