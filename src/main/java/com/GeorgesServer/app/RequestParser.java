7c932fa0200b6f43d524cef346aab046ec7f534c
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-02-21 12:41:01 -0500

    remove old setters, create parseRequestStartLineMethod

--- src/main/java/com/GeorgesServer/app/RequestParser.java
+++ src/main/java/com/GeorgesServer/app/RequestParser.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 public class RequestParser {
 
     private RequestReader requestReader;
-    private String convertedRequest;
     private String method;
     private String url;
     private String httpVersion;
@@ -16,39 +15,29 @@ public class RequestParser {
     }
 
     public HashMap<String, String> parse(BufferedReader reader) {
-        convertedRequest = requestReader.read(reader);
-        setMethod();
-        setUrl();
-        setHttpVersion();
-
         HashMap<String, String> parsedRequest = new HashMap<>();
+        String convertedRequest = requestReader.read(reader);
+        parseRequestStartLine(convertedRequest);
+
         parsedRequest.put("Method", getMethod());
         parsedRequest.put("Url", getUrl());
         parsedRequest.put("HttpVersion", getHttpVersion());
         return parsedRequest;
     }
 
-    public void setMethod() {
+    private void parseRequestStartLine(String convertedRequest) {
         String[] splitRequest = convertedRequest.split(" ");
         this.method = splitRequest[0];
+        this.url = splitRequest[1];
+        this.httpVersion = splitRequest[2];
     }
 
     public String getMethod() {
         return method;
     }
 
-    private void setUrl() {
-        String[] splitRequest = convertedRequest.split(" ");
-        this.url = splitRequest[1];
-    }
-
     public String getUrl() {
         return url;
-    }
-
-    private void setHttpVersion() {
-        String[] splitRequest = convertedRequest.split(" ");
-        this.httpVersion = splitRequest[2];
     }
 
     public String getHttpVersion() {
