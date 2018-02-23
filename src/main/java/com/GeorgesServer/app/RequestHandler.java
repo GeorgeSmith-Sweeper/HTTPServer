0821143f24ea35965d23387eaad1ee17a6bad5c0
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-02-23 17:25:47 -0500

    add hard coded responses for simpleGet and simplePost

--- src/main/java/com/GeorgesServer/app/RequestHandler.java
+++ src/main/java/com/GeorgesServer/app/RequestHandler.java
@@ -4,16 +4,17 @@ import java.util.HashMap;
 
 public class RequestHandler {
     public String handle(HashMap<String, String> clientRequest) {
-        // SIMPLE POST
+
+        String defaultResponse = clientRequest.get("HttpVersion") + " 200 OK" + "\n";
+
         if (clientRequest.get("Method").equals("POST") && clientRequest.get("Url").equals("/form")) {
             return clientRequest.get("HttpVersion") + " 200 OK" + "\n";
         }
 
-        // SIMPLE GET
         if (clientRequest.get("Method").equals("GET") && clientRequest.get("Url").equals("/")) {
             return clientRequest.get("HttpVersion") + " 200 OK" + "\n";
         }
-        return clientRequest.get("HttpVersion") + " 200 OK" + "\n";
 
+        return defaultResponse;
     }
 }
