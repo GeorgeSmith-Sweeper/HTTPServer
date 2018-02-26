6ec6e966e235a147c4c79cb3e1be8ad859549a2f
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-02-26 09:48:47 -0500

    create statusCodes constant file. HardCode simple head response

--- src/main/java/com/GeorgesServer/app/RequestHandler.java
+++ src/main/java/com/GeorgesServer/app/RequestHandler.java
@@ -2,19 +2,26 @@ package com.GeorgesServer.app;
 
 import java.util.HashMap;
 
+
 public class RequestHandler {
     public String handle(HashMap<String, String> clientRequest) {
 
-        String defaultResponse = clientRequest.get("HttpVersion") + " 200 OK" + "\n";
-
         if (clientRequest.get("Method").equals("POST") && clientRequest.get("Url").equals("/form")) {
-            return clientRequest.get("HttpVersion") + " 200 OK" + "\n";
+            return clientRequest.get("HttpVersion") + " " + StatusCodes.OK+ "\n";
         }
 
         if (clientRequest.get("Method").equals("GET") && clientRequest.get("Url").equals("/")) {
-            return clientRequest.get("HttpVersion") + " 200 OK" + "\n";
+            return clientRequest.get("HttpVersion") + " " + StatusCodes.OK + "\n";
         }
 
-        return defaultResponse;
+        if (clientRequest.get("Method").equals("HEAD") && clientRequest.get("Url").equals("/")) {
+            return clientRequest.get("HttpVersion") + " " + StatusCodes.OK + "\n";
+        }
+
+        if (clientRequest.get("Method").equals("HEAD") && clientRequest.get("Url").equals("/foobar")) {
+            return clientRequest.get("HttpVersion") + " " + StatusCodes.NOT_FOUND + "\n";
+        }
+
+        return null;
     }
 }
