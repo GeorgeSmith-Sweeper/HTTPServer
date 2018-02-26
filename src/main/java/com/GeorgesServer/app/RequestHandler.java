8ced759ce1c06c13263fa52e37edcbeb7a76b732
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-02-26 10:29:55 -0500

    remove 3 conditional statements in handler

--- src/main/java/com/GeorgesServer/app/RequestHandler.java
+++ src/main/java/com/GeorgesServer/app/RequestHandler.java
@@ -4,24 +4,10 @@ import java.util.HashMap;
 
 
 public class RequestHandler {
-    StringBuilder sb = new StringBuilder();
 
     public String handle(HashMap<String, String> clientRequest) {
 
-
-        if (clientRequest.get("Method").equals("GET") && clientRequest.get("Url").equals("/")) {
-            return clientRequest.get("HttpVersion") + " " + StatusCodes.OK + "\n";
-        }
-
-        if (clientRequest.get("Method").equals("HEAD") && clientRequest.get("Url").equals("/")) {
-            return clientRequest.get("HttpVersion") + " " + StatusCodes.OK + "\n";
-        }
-
-        if (clientRequest.get("Method").equals("POST") && clientRequest.get("Url").equals("/form")) {
-            return clientRequest.get("HttpVersion") + " " + StatusCodes.OK+ "\n";
-        }
-
-        if (clientRequest.get("Method").equals("PUT") && clientRequest.get("Url").equals("/form")) {
+        if (clientRequest.get("Url").equals("/") || clientRequest.get("Url").equals("/form")) {
             return clientRequest.get("HttpVersion") + " " + StatusCodes.OK + "\n";
         }
 
