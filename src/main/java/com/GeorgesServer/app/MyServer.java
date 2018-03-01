8cfe34dd92d3bf1c64720af4e7320a4510a72b4a
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-03-01 13:27:10 -0500

    wire up router inside of server start

--- src/main/java/com/GeorgesServer/app/MyServer.java
+++ src/main/java/com/GeorgesServer/app/MyServer.java
@@ -5,19 +5,22 @@ import com.GeorgesServer.app.com.GeorgesServer.handler.IHandler;
 public class MyServer {
     private StreamMaker streamMaker;
     private RequestParser requestParser;
-    private IHandler requestHandler;
+    private IHandler handler;
     private ResponseSender responseSender;
+    private Router router;
 
     public MyServer(StreamMaker streamMaker,
                     RequestParser requestParser,
-                    IHandler requestHandler,
+                    IHandler handler,
                     ResponseSender responseSender,
+                    Router router,
                     String publicFolderPath) {
 
         this.streamMaker = streamMaker;
         this.requestParser = requestParser;
-        this.requestHandler = requestHandler;
+        this.handler = handler;
         this.responseSender = responseSender;
+        this.router = router;
     }
 
     public void start() {
@@ -25,7 +28,9 @@ public class MyServer {
         while (!formattedResponse.equals("Bye")) {
             Streams streams = streamMaker.connect();
             ClientRequest clientRequest = requestParser.parse(streams.getIn());
-            ServerResponse serverResponse = requestHandler.handle(clientRequest);
+            IHandler handler = router.route(clientRequest.getMethod(), clientRequest.getUrl());
+
+            ServerResponse serverResponse = handler.handle(clientRequest);
             formattedResponse = serverResponse.format();
             responseSender.send(formattedResponse, streams.getOut());
         }
