6b5544a6a654d7b633b35210800c3e7d706b72bb
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-03-01 10:13:27 -0500

    remove comments

--- src/main/java/com/GeorgesServer/app/com/GeorgesServer/handler/RequestHandler.java
+++ src/main/java/com/GeorgesServer/app/com/GeorgesServer/handler/RequestHandler.java
@@ -18,15 +18,5 @@ public class RequestHandler implements IHandler {
         responseBuilder.buildHttpVersion();
         responseBuilder.buildOkStatus();
         return responseBuilder.getResponse();
-
-//        if (clientRequest.getUrl().equals("/") || clientRequest.getUrl().equals("/form")) {
-//            return clientRequest.getHttpVersion() + " " + StatusCodes.OK + "\n";
-//        }
-////
-//        if (clientRequest.getMethod().equals("HEAD") && clientRequest.getUrl().equals("/foobar")) {
-//            return clientRequest.getHttpVersion() + " " + StatusCodes.NOT_FOUND + "\n";
-//        }
-
-//        return "";
     }
 }
