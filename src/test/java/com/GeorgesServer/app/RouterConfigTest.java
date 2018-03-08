3404fb627d1f86c8507181c8e8cbbb55e8403119
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-03-08 11:36:45 -0500

    add partial content.txt to routerConfig

--- src/test/java/com/GeorgesServer/app/RouterConfigTest.java
+++ src/test/java/com/GeorgesServer/app/RouterConfigTest.java
@@ -3,6 +3,7 @@ package com.GeorgesServer.app;
 import com.GeorgesServer.app.com.GeorgesServer.handler.DefaultHandler;
 import com.GeorgesServer.app.com.GeorgesServer.handler.FormHandler;
 import com.GeorgesServer.app.com.GeorgesServer.handler.IHandler;
+import com.GeorgesServer.app.com.GeorgesServer.handler.PartialContentHandler;
 import com.GeorgesServer.app.com.GeorgesServer.response.HttpResponseBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,15 +28,18 @@ class RouterConfigTest {
     void makeRoutesAssignsAPathToASpecificHandler() {
         IHandler defaultHandler = new DefaultHandler();
         IHandler formHandler = new FormHandler();
+        IHandler partialContentHandler = new PartialContentHandler();
 
         RouterConfig subject = new RouterConfig(mockedRouterHandlers);
 
         when(mockedRouterHandlers.get("defaultHandler")).thenReturn(defaultHandler);
         when(mockedRouterHandlers.get("formHandler")).thenReturn(formHandler);
+        when(mockedRouterHandlers.get("partialContentHandler")).thenReturn(partialContentHandler);
 
         subject.makeRoutes(mockedRouter);
 
         verify(mockedRouter).addRoute("/", defaultHandler);
         verify(mockedRouter).addRoute("/form", formHandler);
+        verify(mockedRouter).addRoute("/partial_content.txt", partialContentHandler);
     }
 }
