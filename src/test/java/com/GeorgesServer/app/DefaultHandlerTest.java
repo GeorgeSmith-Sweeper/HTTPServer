6236200bb3d3a0779c61f938c71e933cf1b49a6e
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-03-14 16:53:49 -0400

    remove unused imports

--- src/test/java/com/GeorgesServer/app/DefaultHandlerTest.java
+++ src/test/java/com/GeorgesServer/app/DefaultHandlerTest.java
@@ -2,15 +2,11 @@ package com.GeorgesServer.app;
 
 import com.GeorgesServer.app.com.GeorgesServer.handler.DefaultHandler;
 import com.GeorgesServer.app.com.GeorgesServer.request.ClientRequest;
-import com.GeorgesServer.app.com.GeorgesServer.response.HttpResponseBuilder;
-import com.GeorgesServer.app.com.GeorgesServer.response.ServerResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 class DefaultHandlerTest {
     private DefaultHandler subject;
