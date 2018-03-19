124f2263acd929ae442de2836fb973576313c585
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-03-19 12:43:35 -0400

    create FourOhFourHandler to pass 404test

---
+++ src/main/java/com/GeorgesServer/app/com/GeorgesServer/handler/FourOhFourHandler.java
@@ -0,0 +1,32 @@
+package com.GeorgesServer.app;
+
+import com.GeorgesServer.app.com.GeorgesServer.handler.IHandler;
+
+import java.util.HashMap;
+
+public class FourOhFourHandler implements IHandler{
+    @Override
+    public void handle() {
+
+    }
+
+    @Override
+    public String getStatus() {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, String> getHeaders() {
+        return null;
+    }
+
+    @Override
+    public String getBody() {
+        return null;
+    }
+
+    @Override
+    public String format() {
+        return null;
+    }
+}
